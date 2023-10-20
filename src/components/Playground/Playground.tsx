d279364347730f53dfd312308620e753aedd7f8d
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2023-10-20 16:16:44 +0300

    created Timer

--- src/components/Playground/Playground.tsx
+++ src/components/Playground/Playground.tsx
@@ -1,15 +1,33 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useAppDispatch, useAppSelector } from "../../app/hooks"
 import { setCurrentStep } from "./store/slices"
+import { INTERVAL_TIME } from "./constants"
 import Controls from "./components/Controls"
 
 const Playground: React.FC = () => {
   const state = useAppSelector((state) => state.playground) //достаем стейт из редьюсера(обьявляли его в store)
   const dispatch = useAppDispatch()
 
+  const refreshIntervalId = useRef<ReturnType<typeof setInterval> | null>(null) //переменная для паузы
+
   const [isTimerActive, setIsTimerActive] = useState<boolean>(false) //не выносим в Controls потому что эти значения будем использовать в других  местах
+  useEffect(() => {
+    if (isTimerActive) {
+      refreshIntervalId.current = setInterval(() => {
+        dispatch(setCurrentStep())
+      }, INTERVAL_TIME)
+    } else {
+      clearInterval(refreshIntervalId.current as NodeJS.Timeout)
+    }
+    return () => {
+      clearInterval(refreshIntervalId.current as NodeJS.Timeout)
+    } //если происходит размонтирование копонента, то нужно очищать таймер
+  }, [isTimerActive, dispatch])
+
   return (
     <div>
+      {state.currentStep}
+
       <Controls
         isTimerActive={isTimerActive}
         setIsTimerActive={setIsTimerActive}
