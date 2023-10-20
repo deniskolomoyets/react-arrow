544cf320a8be362e00e508a5c21a36fe3a545375
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2023-10-20 15:10:16 +0300

    created component Control for future buttons Play/Pause

--- src/components/Playground/Playground.tsx
+++ src/components/Playground/Playground.tsx
@@ -1,18 +1,19 @@
 import { useState } from "react"
 import { useAppDispatch, useAppSelector } from "../../app/hooks"
 import { setCurrentStep } from "./store/slices"
+import Controls from "./components/Controls"
 
 const Playground: React.FC = () => {
   const state = useAppSelector((state) => state.playground) //достаем стейт из редьюсера(обьявляли его в store)
   const dispatch = useAppDispatch()
 
-  const [isTimerActive, setIsTimerActive] = useState<boolean>(false)
+  const [isTimerActive, setIsTimerActive] = useState<boolean>(false) //не выносим в Controls потому что эти значения будем использовать в других  местах
   return (
     <div>
-      {state.currentStep}{" "}
-      <button onClick={() => dispatch(setCurrentStep())}>
-        Change Current step
-      </button>
+      <Controls
+        isTimerActive={isTimerActive}
+        setIsTimerActive={setIsTimerActive}
+      />
     </div>
   )
 }
