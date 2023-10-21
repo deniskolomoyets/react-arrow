cf7e5b9f8ac79595f61f49e7c0e03ff687ee8cda
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2023-10-21 13:44:10 +0300

    created components RandomKeys

--- src/components/Playground/Playground.tsx
+++ src/components/Playground/Playground.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useRef, useState } from "react"
 import { useAppDispatch, useAppSelector } from "../../app/hooks"
+
 import { setCurrentStep } from "./store/slices"
 import { INTERVAL_TIME } from "./constants"
+
 import Controls from "./components/Controls"
+import RandomKeys from "./components/RandomKeys"
 
 const Playground: React.FC = () => {
   const state = useAppSelector((state) => state.playground) //достаем стейт из редьюсера(обьявляли его в store)
@@ -32,6 +35,7 @@ const Playground: React.FC = () => {
         isTimerActive={isTimerActive}
         setIsTimerActive={setIsTimerActive}
       />
+      <RandomKeys isTimerActive={isTimerActive} />
     </div>
   )
 }
---
+++ src/components/Playground/components/RandomKeys/RandomKeys.tsx
@@ -0,0 +1,13 @@
+// import styles from "./RandomKeys.module.css"
+
+export interface IRandomKeysProps {
+  isTimerActive: boolean
+}
+
+const RandomKeys: React.FC<IRandomKeysProps> = (props) => {
+  //   const { isTimerActive } = props
+
+  return <div>RandomKeys</div>
+}
+
+export default RandomKeys
