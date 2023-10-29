86bcdf887d7dafb05274ee8c96dc4297c6acc6b1
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2023-10-29 22:09:10 +0200

    component KeyPressed  stylization

--- src/components/Playground/components/KeyPressed/KeyPressed.tsx
+++ src/components/Playground/components/KeyPressed/KeyPressed.tsx
@@ -1,4 +1,4 @@
-// import styles from "./KeyPressed.module.css"
+import styles from "./KeyPressed.module.css"
 import { useCallback, useEffect } from "react"
 
 import { setEnteredValue } from "../../store/slices"
@@ -32,11 +32,15 @@ const KeyPressed: React.FC<IKeyPressedProps> = (props) => {
   })
   return (
     <div>
-      <TypographyHeader>KeyPressed</TypographyHeader>
-      <TypographyText>
-        Press the key corresponding to the key in "Random keys"
-      </TypographyText>
-      <span>{keyPressedElement}</span>
+      <TypographyHeader>Key pressed</TypographyHeader>
+      <div className={styles.container}>
+        <TypographyText>
+          Press the key corresponding to the key in "Random keys"
+        </TypographyText>
+        <div className={styles.wrapper}>
+          <span className={styles.icon}>{keyPressedElement}</span>
+        </div>
+      </div>
     </div>
   )
 }
