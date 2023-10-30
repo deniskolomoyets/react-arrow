8a21e7611ae03db37f7bcca22bcfce801d89fb28
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2023-10-30 20:54:09 +0200

    test reducer setEnteredValue, totalSuccessful and totalUnsuccessful

--- src/components/Playground/store/__test__/setEnteredValue.spec.ts
+++ src/components/Playground/store/__test__/setEnteredValue.spec.ts
@@ -1,8 +1,44 @@
-// import playgroundReducer, { initialState } from "../slices"
+import { ARR_ARROW_CODES } from "../../constants"
+import playgroundReducer, {
+  initialState,
+  setCurrentStep,
+  setEnteredValue,
+  setSteps,
+} from "../slices"
 
-// describe("reducer setSomeReducer", () => {
-//   it("check setSomeReducer", () => {
-//     const state = playgroundReducer({}, {})
-//     expect().toEqual()
-//   })
-// })
+describe("reducer setEnteredValue", () => {
+  it("check enteredValue", () => {
+    const enteredValue = ARR_ARROW_CODES[0]
+    const setCurrentStepState = playgroundReducer(
+      initialState,
+      setCurrentStep(),
+    )
+
+    const setStepsState = playgroundReducer(setCurrentStepState, setSteps())
+
+    const setEnteredValueState = playgroundReducer(
+      setStepsState,
+      setEnteredValue(enteredValue),
+    )
+
+    expect(setStepsState.steps[0].enteredValue).toBe(null)
+    expect(setEnteredValueState.steps[0].enteredValue).toBe(enteredValue)
+  })
+  it("check totalSuccessful and totalUnsuccessful", () => {
+    const setCurrentStepState = playgroundReducer(
+      initialState,
+      setCurrentStep(),
+    )
+
+    const setStepsState = playgroundReducer(setCurrentStepState, setSteps())
+
+    const setEnteredValueState = playgroundReducer(
+      setStepsState,
+      setEnteredValue(setStepsState.steps[0].currentValue), //чтобы currentValue совпадал с enteredValue
+    )
+    expect(setStepsState.totalSuccessful).toBe(0)
+    expect(setStepsState.totalUnsuccessful).toBe(0)
+    expect(setEnteredValueState.totalSuccessful).toBe(1)
+    expect(setEnteredValueState.totalUnsuccessful).toBe(0)
+  })
+})
